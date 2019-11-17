7c190bb6d4cc8eb50be6c4bd5f6d43caf8006b79
Rahul Prasad <rahul@MacBook-Pro-9.local>
2019-11-16 23:20:07 -0800

    added basic framework for interactive map

--- temp.R
+++ temp.R
@@ -28,7 +28,7 @@ ggplot(world_shape) +
   ) +
   coord_map() +
   scale_fill_continuous(low = "#132B43", high = "Red") +
-  labs(fill = "Happiness Score (0-5)")
+  labs(fill = "Happiness Score")
 
 
 
