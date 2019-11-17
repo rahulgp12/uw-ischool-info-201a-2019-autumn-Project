132fa2b973ba3937b0b66cdde6c634bec42ab388
Pasit Areepipatkul <pasit.aree@gmail.com>
2019-11-16 23:16:26 -0800

    made UI and included graph

--- temp.R
+++ temp.R
@@ -15,5 +15,18 @@ ggplot(data_2017, aes(x = Freedom, y = Happiness.Score)) +
   geom_smooth(method = "lm", se = FALSE) +
   ggtitle("Comparing Effect of Freedom on Happiness Score")
 
-
+# Creates an un-interactive heatmap of the world using happiness scores
+world_shape <- map_data("world") %>%      # Creates data table holding all countries
+  rename(Country = region) %>%
+  left_join(data_2017, by = "Country")    # Adds Happiness score data to map 
+ggplot(world_shape) +
+  geom_polygon(
+    mapping = aes(x = world_shape$long, y = world_shape$lat, 
+                  group = world_shape$group, fill = world_shape$Happiness.Score),
+    color = "white", # shows country outlines
+    size = 0.1       # thin border
+  ) +
+  coord_map() +
+  scale_fill_continuous(low = "#132B43", high = "Red") +
+  labs(fill = "Happiness Score (0-5)")
 
