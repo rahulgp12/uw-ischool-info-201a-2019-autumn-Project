875684de52e79511e2f67e62a097a1f31f15c1fb
Pasit Areepipatkul <pasit.aree@gmail.com>
2019-11-20 20:04:29 -0800

    tabs

--- temp.R
+++ temp.R
@@ -4,6 +4,9 @@ data_2017 <- read.csv('data/2017.csv', stringsAsFactors = FALSE)
 
 install.packages('ggplot2')
 
+print(colnames(data_2017))
+c()
+
 ggplot(data_2017, aes(x = Family, y = Happiness.Score)) +
   geom_point() +
   geom_smooth(method = "lm", se = FALSE) +
@@ -18,16 +21,15 @@ ggplot(data_2017, aes(x = Freedom, y = Happiness.Score)) +
 # Creates an un-interactive heatmap of the world using happiness scores
 world_shape <- map_data("world") %>%      # Creates data table holding all countries
   rename(Country = region) %>%
-  left_join(data_2017, by = "Country")    # Adds Happiness score data to map
+  left_join(data_2017, by = "Country")    # Adds Happiness score data to map 
 ggplot(world_shape) +
   geom_polygon(
-    mapping = aes(x = world_shape$long, y = world_shape$lat,
+    mapping = aes(x = world_shape$long, y = world_shape$lat, 
                   group = world_shape$group, fill = world_shape$Happiness.Score),
     color = "white", # shows country outlines
     size = 0.1       # thin border
   ) +
   coord_map() +
   scale_fill_continuous(low = "#132B43", high = "Red") +
-  labs(fill = "Happiness Score (0-10)") +
-  xlab("Longitude") +
-  ylab("Latitude")
+  labs(fill = "Happiness Score (0-5)")
+
--- ui.R
+++ ui.R
@@ -1,22 +1,29 @@
 
 library("shiny")
+library("dplyr")
 
 data_2017 <- read.csv('data/2017.csv', stringsAsFactors = FALSE)
+desired_columns <- select(data_2017, Economy..GDP.per.Capita., Family, Health..Life.Expectancy.,
+                          Freedom, Generosity, Trust..Government.Corruption.)
 
-shinyServer(
-  pageWithSidebar(
-    headerPanel("Global Happiness Report"),
-    
-    # Ideally if the CSV file is stored as a dataframe, we want to take all the
-    # column names and put it down here.
-    sidebarPanel(
-      selectInput("Feature", "Please Select Feature to Compare",
-                  choices = colnames(data_2017)),
-    ),
-    
-    mainPanel(
-      plotOutput("myPlot")
-    )
-    
-  )
-)
+shinyUI(navbarPage(title = "Global Happiness Report",
+                   tabPanel("Scatterplot 1",
+                            shinyServer(
+                              pageWithSidebar(
+                                headerPanel("Global Happiness Report"),
+                                sidebarPanel(
+                                  selectInput("Feature", "Please Select Feature to Compare",
+                                              choices = colnames(desired_columns)),
+                                ),
+                                
+                                mainPanel(
+                                  plotOutput("myPlot")
+                                )
+                                
+                              )
+                            )
+                   ),
+                   tabPanel("Scatterplot 2"),
+                   tabPanel("Heatmap"),
+                   tabPanel("Table")
+))
